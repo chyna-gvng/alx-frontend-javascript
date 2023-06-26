9b235988f1d3277ae6fc7c56fc7698abaafd7b4e
Walter <w.angoye@alustudent.com>
2023-06-26 15:37:58 +0300

    Updated 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
 import getBudgetObject from './7-getBudgetObject.js';
- /* eslint-disable */
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
