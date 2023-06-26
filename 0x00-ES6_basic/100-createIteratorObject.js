a49c7a004c34a27ef55a92c0cd1fc1c59e37c7fc
Walter <w.angoye@alustudent.com>
2023-06-26 15:40:18 +0300

    Updated 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,29 @@
+export default function createIteratorObject(report) {
+    let currentDepartmentIndex = 0;
+    let currentEmployeeIndex = 0;
+  
+    const departments = Object.values(report);
+  
+    return {
+      next() {
+        if (currentDepartmentIndex >= departments.length) {
+          return { done: true };
+        }
+  
+        const department = departments[currentDepartmentIndex];
+        const employees = department.employees;
+  
+        if (currentEmployeeIndex >= employees.length) {
+          currentDepartmentIndex++;
+          currentEmployeeIndex = 0;
+          return this.next();
+        }
+  
+        const employee = employees[currentEmployeeIndex];
+        currentEmployeeIndex++;
+  
+        return { value: employee, done: false };
+      }
+    };
+  }
+  
