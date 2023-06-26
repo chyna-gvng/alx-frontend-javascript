936c3178d0ea547b16668bf38bcab23c5536900c
Walter <w.angoye@alustudent.com>
2023-06-26 15:41:50 +0300

    Updated 101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,13 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let result = '';
+  
+    for (const employee of reportWithIterator) {
+      result += `${employee} | `;
+    }
+  
+    // Remove the trailing ' | ' from the string
+    result = result.slice(0, -3);
+  
+    return result;
+  }
+  
