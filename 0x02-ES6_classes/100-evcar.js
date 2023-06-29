36c4a91658c7ea4a08023c4f2caba26a611ff0a8
Walter <w.angoye@alustudent.com>
2023-06-29 18:41:39 +0300

    Updated 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,19 +1,17 @@
+/*eslint-disable*/
 import Car from './10-car';
 
-// eslint-disable-next-line camelcase
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
 
-    // eslint-disable-next-line no-unused-vars
     this._range = range;
   }
 
   cloneCar() {
-    // eslint-disable-next-line no-console
-    console.log('Cloning car...');
     const NewObj = this.constructor[Symbol.species] || this.constructor;
     const clone = new NewObj();
     return clone;
   }
 }
+/*eslint-enable*/
