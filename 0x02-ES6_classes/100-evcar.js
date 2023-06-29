1ef2855df4f90cfda8ca132eca739d94e737dd10
Walter <w.angoye@alustudent.com>
2023-06-29 18:48:53 +0300

    Updated 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,24 @@
-/* eslint-disable no-underscore-dangle */
-// eslint-disable-next-line import/no-unresolved
 import Car from './10-car';
+
+/* eslint-disable no-underscore-dangle */
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
+    // call constructor of super class (Building)
     super(brand, motor, color);
+
+    // Create objs
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  // Methods
+
+  cloneCar() {
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
   }
+
 }
+// eslint-disable-next-line import/no-unresolved
+
