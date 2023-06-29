29348551b02d031d5726ab1c43d39408eb1fdf19
Walter <w.angoye@alustudent.com>
2023-06-29 13:37:52 +0300

    Updated 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,26 +1,18 @@
-export default class Car {
+class Car {
     constructor(brand, motor, color) {
-      // Create objs
       this._brand = brand;
       this._motor = motor;
       this._color = color;
     }
   
-    // Methods
-  
     cloneCar() {
-      const NewObj = this.constructor[Symbol.species] || this.constructor;
-      const clone = new NewObj();
-      return clone;
+      const car = new Car();
+      Object.getOwnPropertySymbols(this).forEach((symbol) => {
+        car[symbol] = this[symbol];
+      });
+      return car;
     }
-  
-    // Setters
-  
-    // Getters
   }
-  /* class TestCar extends Car {};
-  const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-  const tc2 = tc1.cloneCar();
-  console.log(tc1 instanceof TestCar);
-  console.log(tc2 instanceof TestCar);
-  console.log(tc1 == tc2); */
+  
+  export default Car;
+  
