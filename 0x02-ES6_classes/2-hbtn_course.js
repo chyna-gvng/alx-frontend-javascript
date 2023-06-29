e12ff364cd4874bcc057f6a0707b168fcbd9e951
Walter <w.angoye@alustudent.com>
2023-06-29 17:54:02 +0300

    Updated 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -11,16 +11,16 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
+  set name(newName) {
+    if (Object.getPrototypeOf(newName) !== String.prototype) throw TypeError('name must be a string');
+    this._name = newName;
+  }
   
-    set name(newName) {
-      if (Object.getPrototypeOf(newName) !== String.prototype) throw TypeError('name must be a string');
-      this._name = newName;
-    }
-  
-    set length(newLen) {
-      if (Object.getPrototypeOf(newLen) !== Number.prototype) throw TypeError('length must be a number');
-      this._length = newLen;
-    }
+  set length(newLen) {
+    if (Object.getPrototypeOf(newLen) !== Number.prototype) throw TypeError('length must be a number');
+    this._length = newLen;
+  }
   
     set students(newStudents) {
       if (Object.getPrototypeOf(newStudents) !== Array.prototype) throw TypeError('students must be an array');
