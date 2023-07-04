efbda92bc5680972f89408a244a6e51d6d0705a0
Walter <w.angoye@alustudent.com>
2023-07-04 14:09:12 +0300

    Updated 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,4 @@
 export default function cleanSet(set, startString) {
-  // eslint-disable-next-line
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
   return [...set]
     .filter((el) => el.startsWith(startString))
