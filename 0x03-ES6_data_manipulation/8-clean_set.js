634f6cb54e3fa07597cc8edcfc344aa5cc833dd7
Walter <w.angoye@alustudent.com>
2023-07-04 14:03:42 +0300

    Updated 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+function cleanSet(set, startString) {
+  const filteredValues = [...set].filter(value => value.startsWith(startString));
+  return filteredValues.join('-');
 }
+
+export default cleanSet;
+
+
