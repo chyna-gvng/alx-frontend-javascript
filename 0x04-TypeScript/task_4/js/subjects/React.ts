779f6ea4c574f4b346a68b6f3de24e05d2f01a98
Walter <w.angoye@alustudent.com>
2023-07-05 12:24:54 +0300

    Updated React.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,15 @@
+  namespace Subjects {
+    class React extends Subjects.Subject {
+      getRequirements() {
+        return "Here is the list of requirements for React";
+      }
+  
+      getAvailableTeacher() {
+        if (this.teacher.experienceTeachingReact !== undefined) {
+          return `Available Teacher: ${this.teacher.firstName}`;
+        } else {
+          return "No available teacher";
+        }
+      }
+    }
+  }
