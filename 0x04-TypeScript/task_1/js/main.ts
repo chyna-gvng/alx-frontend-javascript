03040946e0625eff6ae3cb9e7762e458a3b69df0
Walter <w.angoye@alustudent.com>
2023-07-05 11:57:05 +0300

    Updated main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,10 +1,14 @@
 interface Teacher {
-  firstName: string;
-  lastName: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [propName: string]: any;
+  [propName : string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
 const teacher3: Teacher = {
@@ -14,5 +18,46 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
-
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: false,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+function printTeacher(firstName: string, lastName:string): string {
+  return `${firstName[0]}.${lastName}`;
+}
+console.log(printTeacher("John", "Doe"));
+
+interface interfaceClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface constructClass {
+  new(firstName: string, lastName: string): interfaceClass;
+}
+
+class StudentClass implements interfaceClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
