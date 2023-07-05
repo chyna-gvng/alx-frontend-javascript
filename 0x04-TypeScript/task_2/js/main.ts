b2e0d8c27d5fabeae58aa7d31031f3b089726a79
Walter <w.angoye@alustudent.com>
2023-07-05 12:00:10 +0300

    Updated main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,39 +1,66 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-const firstStudent: Student = {
-  firstName: 'Chyna',
-  lastName: 'Gvng',
-  age: 18,
-  location: 'Beijing',
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
 }
 
-const secondStudent: Student = {
-  firstName: 'Vicks',
-  lastName: 'Batch',
-  age: 16,
-  location: 'Mogadishu',
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
 }
 
-const studentsList: Array<Student> = [firstStudent, secondStudent]
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
 
-const newTable = document.createElement('table');
-const bodyTable = document.createElement('body');
+function createEmployee(firstName: string, lastName: string, salary: number | string ): Director | Teacher {
+  if(salary as number && salary < 500) {
+    return new Teacher();
+  }
+  else {
+    return new Director();
+  }
+}
 
-studentsList.forEach((obj) => {
-  const myrow = document.createElement('row');
-  const cellstName = document.createElement('tfn');
-  const cellLocation = document.createElement('tlc');
-  cellstName.textContent = obj.firstName;
-  cellLocation.textContent =obj.location;
-  myrow.appendChild(cellstName);
-  myrow.appendChild(cellLocation);
-  bodyTable.appendChild(myrow);
-});
+console.log(createEmployee('Camilo ', 'Torres ', 200));
+console.log(createEmployee('Simon ', 'Bolivar ', 1000));
+console.log(createEmployee('Antonio ','Nariño ', '$500'));
 
-newTable.appendChild(bodyTable);
-document.body.appendChild(newTable);
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee instanceof Director) ? true : false;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  return isDirector(employee) ? employee.workDirectorTasks() : employee.workTeacherTasks();
+}
+console.log(executeWork(createEmployee('Camilo ', 'Torres ', 200)));
+console.log(executeWork(createEmployee('Simon  ', 'Bolivar ', 1000)));
+
+type Subjects = 'Math' | 'History';
+function teachClass(todayClass: Subjects): string {
+  return todayClass === 'Math'? 'Teaching Math' : 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
