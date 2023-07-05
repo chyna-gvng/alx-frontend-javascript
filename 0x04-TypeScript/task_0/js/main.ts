5c808d9af7163510c04cf7c2d525e37acdc89760
Walter <w.angoye@alustudent.com>
2023-07-05 11:54:09 +0300

    Updated main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -5,18 +5,35 @@ interface Student {
   location: string;
 }
 
-const student1: Student = {
-  firstName: "Chyna",
-  lastName: "Gvng",
-  age: 20,
-  location: "Beijing",
-};
-
-const student2: Student = {
-  firstName: "Vicks",
-  lastName: "Batch",
+const firstStudent: Student = {
+  firstName: 'Chyna',
+  lastName: 'Gvng',
   age: 18,
-  location: "Nakuru",
-};
+  location: 'Beijing',
+}
 
-const studentsList: Student[] = [student1, student2];
+const secondStudent: Student = {
+  firstName: 'Vicks',
+  lastName: 'Batch',
+  age: 16,
+  location: 'Mogadishu',
+}
+
+const studentsList: Array<Student> = [firstStudent, secondStudent]
+
+const newTable = document.createElement('table');
+const bodyTable = document.createElement('body');
+
+studentsList.forEach((obj) => {
+  const myrow = document.createElement('row');
+  const cellstName = document.createElement('tfn');
+  const cellLocation = document.createElement('tlc');
+  cellstName.textContent = obj.firstName;
+  cellLocation.textContent =obj.location;
+  myrow.appendChild(cellstName);
+  myrow.appendChild(cellLocation);
+  bodyTable.appendChild(myrow);
+});
+
+newTable.appendChild(bodyTable);
+document.body.appendChild(newTable);
