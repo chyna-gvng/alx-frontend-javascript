2de343854aab235e478366d0c250914c379c8c67
Walter <w.angoye@alustudent.com>
2023-07-05 12:36:25 +0300

    Updated main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,39 +1,14 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+export interface MajorCredits {
+  credits: number;
 }
 
-const firstStudent: Student = {
-  firstName: 'Chyna',
-  lastName: 'Gvng',
-  age: 18,
-  location: 'Beijing',
+export interface MinorCredits {
+  credits: number;
 }
 
-const secondStudent: Student = {
-  firstName: 'Vicks',
-  lastName: 'Batch',
-  age: 16,
-  location: 'Mogadishu',
+function sumMajorCredits(subject1: MajorCredits, subject2:MajorCredits): number{
+  return subject1.credits + subject2.credits;
 }
-
-const studentsList: Array<Student> = [firstStudent, secondStudent]
-
-const newTable = document.createElement('table');
-const bodyTable = document.createElement('body');
-
-studentsList.forEach((obj) => {
-  const myrow = document.createElement('row');
-  const cellstName = document.createElement('tfn');
-  const cellLocation = document.createElement('tlc');
-  cellstName.textContent = obj.firstName;
-  cellLocation.textContent =obj.location;
-  myrow.appendChild(cellstName);
-  myrow.appendChild(cellLocation);
-  bodyTable.appendChild(myrow);
-});
-
-newTable.appendChild(bodyTable);
-document.body.appendChild(newTable);
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number{
+  return subject1.credits + subject2.credits;
+}
