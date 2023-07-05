0f060211fe8724bc05a6e4d7be7597c9fc8c28e5
Walter <w.angoye@alustudent.com>
2023-07-05 12:30:35 +0300

    Updated Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,5 +1,5 @@
-  namespace Subjects {
-    class Cpp extends Subjects.Subject {
+namespace Subjects {
+    export class Cpp extends Subjects.Subject {
       getRequirements() {
         return "Here is the list of requirements for Cpp";
       }
@@ -13,3 +13,4 @@
       }
     }
   }
+  
