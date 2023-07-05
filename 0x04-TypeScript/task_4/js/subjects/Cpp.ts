ba6cec463d3f88f66f53de6e4711a0a03b361d65
Walter <w.angoye@alustudent.com>
2023-07-05 12:24:06 +0300

    Updated Cpp.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,15 @@
+  namespace Subjects {
+    class Cpp extends Subjects.Subject {
+      getRequirements() {
+        return "Here is the list of requirements for Cpp";
+      }
+  
+      getAvailableTeacher() {
+        if (this.teacher.experienceTeachingC !== undefined) {
+          return `Available Teacher: ${this.teacher.firstName}`;
+        } else {
+          return "No available teacher";
+        }
+      }
+    }
+  }
